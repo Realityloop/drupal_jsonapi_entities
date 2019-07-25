2c752f92f4efddf97cc6f5ffd351cf9917a2f6e1
Stuart Clark <stu@rtclark.net>
2019-07-25 10:56:06 +1000

    [#1] Added getSchema class and basic validation.

--- src/getFormSchema.js
+++ src/getFormSchema.js
@@ -1,71 +1,24 @@
+import getSchema from './getSchema'
 import entityFormDisplay from './resources/entityFormDisplay'
 import fieldConfig from './resources/fieldConfig'
 import fieldStorageConfig from './resources/fieldStorageConfig'
 
-class getFormSchema {
+class getFormSchema extends getSchema {
   constructor(entityType, bundle, mode, context) {
-    this.entityType = entityType
-    this.bundle = bundle
-    this.mode = mode
-    this.headers = context.root.headers
+    super(entityType, bundle, mode, context)
 
-    this.entityFormDisplay = new entityFormDisplay(this)
-    this.fieldConfig = new fieldConfig(this)
-    this.fieldStorageConfig = new fieldStorageConfig(this)
+    this.resources = [
+      new entityFormDisplay(this),
+      new fieldConfig(this),
+      new fieldStorageConfig(this)
+    ]
+  }
 
-    this.schema = {
-      fields: {},
-      groups: {},
-      keys: []
+  validate(data) {
+    const entityFormDisplay = data[this.resources[0].requestId]
+    if (entityFormDisplay.length === 0) {
+      throw new Error(`Entity type invalid: ${this.entityType} ${this.bundle}`)
     }
-  }
-
-  process(data) {
-    this.entityFormDisplay.process(data[this.entityFormDisplay.requestId])
-    this.fieldConfig.process(data[this.fieldConfig.requestId])
-    this.fieldStorageConfig.process(data[this.fieldStorageConfig.requestId])
-  }
-
-  fieldAdd(field, data) {
-    if (!this.fieldExists(field)) {
-      this.schema.keys.push(field)
-      this.schema.fields[field] = data
-    } else {
-      // Remove settings, they're merged seperately.
-      const settings = data.settings
-      delete data.settings
-
-      this.schema.fields[field] = Object.assign(this.schema.fields[field], data)
-      this.schema.fields[field].settings = Object.assign(this.schema.fields[field].settings, settings)
-    }
-  }
-
-  fieldExists(field) {
-    return typeof this.schema.fields[field] !== 'undefined'
-  }
-
-  groupAdd(group, data) {
-    this.schema.groups[group] = data
-  }
-
-  get schemaSorted() {
-    return {
-      fields: this.sortByWeight(this.schema.fields),
-      groups: this.sortByWeight(this.schema.groups),
-      keys: this.schema.keys
-    }
-  }
-
-  sortByWeight(data) {
-    const dataArray = []
-
-    for (const item in data) {
-      dataArray.push(data[item])
-    }
-
-    return dataArray.sort((a, b) => {
-      return a.weight - b.weight
-    })
   }
 }
 
@@ -74,9 +27,9 @@ export default async (entityType, bundle, mode, context) => {
 
   // Build the subrequests.
   const subrequests = []
-  subrequests.push(formSchema.entityFormDisplay.subrequest)
-  subrequests.push(formSchema.fieldConfig.subrequest)
-  subrequests.push(formSchema.fieldStorageConfig.subrequest)
+  for (const resource of formSchema.resources) {
+    subrequests.push(resource.subrequest)
+  }
 
   // Execute the subrequests.
   // @TODO - Make Subrequests a library.
